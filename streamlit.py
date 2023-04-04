6f92058e40606efbbcf32e75dbeb2e4007dde974
willysim <picard.sim@gmail.com>
2023-04-04 20:36:18 +0200

    streamlit v0.2

--- streamlit.py
+++ streamlit.py
@@ -9,39 +9,31 @@ st.title('Project Wildfire :fire:')
 my_upload = st.file_uploader('Upload your picture here', type=["png", "jpg", "jpeg"])
 col1, col2 = st.columns(2)
 
-# Return GPS informations
-def get_gps(image):
+# return GPS informations
+def display_image_location(image):
     data = dict()
     exif = image.getexif()
     if exif:
-        # GPS Info
-        ifd = exif.get_ifd(0x8825)
+        ifd = exif.get_ifd(0x8825) # retrieve gps exif infos
         for key, val in ifd.items():
             data[ExifTags.GPSTAGS[key]] = val
-    lat =  data['GPSLatitudeRef'], data['GPSLatitude']
-    long = data['GPSLongitudeRef'], data['GPSLongitude']
-    latitude = degrees_to_decimals(lat)
-    longitude = degrees_to_decimals(long)
-    return latitude, longitude
+    coordinates = degrees_to_decimals((data['GPSLatitudeRef'], data['GPSLatitude'])), degrees_to_decimals((data['GPSLongitudeRef'], data['GPSLongitude']))
 
-# Display folium map
-def display_map(coordinates):
-    map = folium.Map(location=[coordinates[0], coordinates[1]], zoom_start=16)
+    map = folium.Map(location=coordinates, zoom_start=16)
     folium.Marker(
-        [coordinates[0], coordinates[1]], popup="Picture location", tooltip="Picture location"
+        coordinates, popup="Fire location", tooltip="Fire location"
     ).add_to(map)
-    st_folium(map, width=725,returned_objects=[])
-
-# Convert GPS to decimals degrees
-def degrees_to_decimals(geo_coord):
-    direction =  geo_coord[0]
-    degrees, minutes, seconds = [geo_coord[1][i] for i in range(3)]
-    d_t_d = float(degrees) + float(minutes)/60 + float(seconds)/3600
+    st_folium(map, width=725)
+    
+# convert GPS to decimals degrees
+def degrees_to_decimals(degrees_coord):
+    direction, (degrees, minutes, seconds) = degrees_coord[0], degrees_coord[1]
+    dtd = float(degrees) + float(minutes)/60 + float(seconds)/3600
     if direction in ('S','W'):
-        d_t_d*= -1
-    return d_t_d
+        dtd*= -1
+    return dtd
 
-# Display original uploaded image and GPS coordinates
+# display original uploaded image and GPS coordinates
 def display_img_gps_location(upload):
     image = Image.open(upload)
 
@@ -50,8 +42,8 @@ def display_img_gps_location(upload):
         st.image(upload)
     
     with col2:
-        st.header('Fire localisation')
-        display_map(get_gps(image))
+        st.header('Fire location')
+        display_image_location(image)
     
 if my_upload is not None:
     display_img_gps_location(my_upload)
