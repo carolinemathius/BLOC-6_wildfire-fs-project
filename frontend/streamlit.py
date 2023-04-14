36f864f9f877c57d5a5101ba877d7e92c94d6158
Simon P <picard.sim@gmail.com>
2023-04-14 14:30:21 +0200

    app 1.3
    reworked ui

--- frontend/streamlit.py
+++ frontend/streamlit.py
@@ -8,24 +8,34 @@ if __name__ == '__main__':
 
     # Create a sidebar on the page and create a select box to choose the detection method
     st.write("<h1 style='text-align: center;'>🔥 Project Wildfire 🔥</h1>", unsafe_allow_html=True)
-    st.divider()
-    st.subheader('Welcome to our Wildfire application! '
-             'Please upload a picture in our app and test our model. '
-             'If there is fire or smoke on your picture, '
-             'our model will detect it, give back the GPS coordinates '
-             'and give you some information about weather forecast '
-             'to help the emergency services assess the situation.')
-    st.divider()
-    st.sidebar.header('Fire detection module') 
-    option = st.sidebar.selectbox('Chose your detection method',('File upload', 'Phone camera'))
+    with st.sidebar:
+        st.header('Fire detection module') 
+        option = st.selectbox('Chose your detection method',('File upload', 'Phone camera'))
 
-    # If the 'File upload' option is selected, create a file uploader 
-    if option == 'File upload':
-        my_upload = st.sidebar.file_uploader('Upload your file',type=["png", "jpg", "jpeg"], label_visibility = 'visible')
+        # If the 'File upload' option is selected, create a file uploader 
+        if option == 'File upload':
+            my_upload = st.file_uploader('Upload your file',type=["png", "jpg", "jpeg"], label_visibility = 'visible')
 
-    # If the 'Phone camera' option is selected, create a camera input
-    if option == 'Phone camera':
-        my_upload = st.sidebar.camera_input('Take a picture')
+        # If the 'Phone camera' option is selected, create a camera input
+        if option == 'Phone camera':
+            my_upload = st.camera_input('Take a picture')
+        st.divider()
+        st.markdown('Welcome to our Wildfire application!')
+        st.markdown('Please upload a picture in our app and test our model. '
+                    'If there is fire or smoke on your picture, '
+                    'our model will detect it.')
+        st.markdown('If there are exploitable exif data on your picture, '
+                    'our application will display the gps location as well as '
+                    'wind related weather, to potentially '
+                    'help emergency services assess the situation.')
+        st.divider()
+        st.markdown('**Wildfire project by :**')
+        st.markdown('[_Anas Maghous_](%s)'%'https://www.linkedin.com/in/anas-maghous/')
+        st.markdown('[_Caroline Mathius_](%s)'%'https://www.linkedin.com/in/carolinemathius/')
+        st.markdown('[_Simon Picard_](%s)'%'https://www.linkedin.com/in/simon-p-64371968/')
+        st.markdown('[_Thibaut Longchamps_](%s)'%'https://www.linkedin.com/in/thibaut-longchamps-0922525a/')
+        st.divider()
+        st.markdown('[**Link to our Github**](%s)'%'https://github.com/carolinemathius/wildfire-fs-project')
 
     # If the file uploader or camera input returns a file, display the results of the detection model, the GPS coordinates and the weather information in separate containers
     if my_upload is not None:
@@ -40,3 +50,12 @@ if __name__ == '__main__':
         with st.container():
             st.header('⛅ Weather informations')
             weather_info(my_upload)
+
+# Remove Made with Streamlit footer and top right burger menu
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
